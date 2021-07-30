9fc47d7a93b647e4c54fda68f992252cff3d92ea
Christian Alamillo <calam002@ucr.edu>
2021-07-29 22:17:39 -0700

    Implemented button in pending project div to generate hard hat image

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const hatButton = document.querySelector('#hat-button');
+const hatImage = document.querySelector('#hat-image');
+
+hatButton.onclick = function() {
+    hatImage.style.display = 'block';
+}
