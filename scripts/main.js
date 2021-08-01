f559a347f02c8d592b89eb585f801f2c6db31935
Christian Alamillo <calam002@ucr.edu>
2021-07-31 20:30:34 -0700

    Fixed some styling bugs and implemented functionality to allow user to toggle between display hat image

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,14 @@
 const hatButton = document.querySelector('#hat-button');
 const hatImage = document.querySelector('#hat-image');
+hatImage.style.display = 'none';
 
 hatButton.onclick = function() {
-    hatImage.style.display = 'block';
+    if (hatImage.style.display === 'none') {
+        hatImage.style.display = 'block';
+        hatButton.innerText = 'Return Hat';
+    }
+    else {
+        hatImage.style.display = 'none';
+        hatButton.innerText = 'Generate Hat';
+    }
 }
