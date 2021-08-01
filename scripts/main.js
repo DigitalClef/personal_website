2a6f66d7aa119d47732f5a25173fa5b52b3c6b31
Christian Alamillo <calam002@ucr.edu>
2021-08-01 09:38:25 -0700

    Changed background-image gradient of project pending div and implemented toggling between colors

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
 const hatButton = document.querySelector('#hat-button');
 const hatImage = document.querySelector('#hat-image');
+const pendingProject = document.querySelector('#project-pending');
 hatImage.style.display = 'none';
 
 hatButton.onclick = function() {
     if (hatImage.style.display === 'none') {
         hatImage.style.display = 'block';
         hatButton.innerText = 'Return Hat';
+        pendingProject.style.backgroundImage = 'linear-gradient(145deg, rgb(198, 238, 198), lightgreen, rgb(198, 238, 198))';
     }
     else {
         hatImage.style.display = 'none';
         hatButton.innerText = 'Generate Hat';
+        pendingProject.style.backgroundImage = 'linear-gradient(145deg, orange, rgb(255, 255, 104), orange)';
     }
 }
