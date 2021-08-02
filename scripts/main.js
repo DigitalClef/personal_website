ec71c78c5b6b6719befe0825781b0ecfd9b8e205
Christian Alamillo <calam002@ucr.edu>
2021-08-01 18:49:49 -0700

    Implemented keyframes animations to hat image, pending div, and hat button

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,13 @@
 const hatButton = document.querySelector('#hat-button');
 const hatImage = document.querySelector('#hat-image');
 const pendingProject = document.querySelector('#project-pending');
-hatImage.style.display = 'none';
+
+hatImage.style.animationPlayState = 'paused';
+pendingProject.style.animationPlayState = 'paused';
+hatButton.style.animationPlayState = 'paused';
 
 hatButton.onclick = function() {
-    if (hatImage.style.display === 'none') {
-        hatImage.style.display = 'block';
-        hatButton.innerText = 'Return Hat';
-        pendingProject.style.backgroundImage = 'linear-gradient(145deg, rgb(198, 238, 198), lightgreen, rgb(198, 238, 198))';
-    }
-    else {
-        hatImage.style.display = 'none';
-        hatButton.innerText = 'Generate Hat';
-        pendingProject.style.backgroundImage = 'linear-gradient(145deg, orange, rgb(255, 255, 104), orange)';
-    }
+    hatImage.style.animationPlayState = 'running';
+    pendingProject.style.animationPlayState = 'running';
+    hatButton.style.animationPlayState = 'running';
 }
