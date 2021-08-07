a8efbc67d4e9d42c58ca2ce5f74f11eeb3b2437a
Christian Alamillo <calam002@ucr.edu>
2021-08-06 19:56:52 -0700

    Implemented transition animation to activate when bottom of document is reached

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const hatButton = document.querySelector('#hat-button');
 const hatImage = document.querySelector('#hat-image');
 const pendingProject = document.querySelector('#project-pending');
+const footerText = document.querySelector('#footer-text');
 
 hatImage.style.animationPlayState = 'paused';
 pendingProject.style.animationPlayState = 'paused';
@@ -10,4 +11,10 @@ hatButton.onclick = function() {
     hatImage.style.animationPlayState = 'running';
     pendingProject.style.animationPlayState = 'running';
     hatButton.style.animationPlayState = 'running';
+}
+
+window.onscroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        footerText.style.fontSize = '20px';
+    }
 }
